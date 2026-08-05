52f9788f550309c67a694e72eddad59468146a3e
Mahmoud AlQataa <malqataa2@gmail.com>
2026-08-05 18:01:28 +0300

    Edit: retybe the front-end

---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+// Expense Tracker Lite - main JS
+// It is currently empty; we will use it later for any additional interactions (such as multi-analysis or charts).
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Expense Tracker Lite loaded.");
+});
