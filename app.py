7cb686a4903508ca062c3a57b9e6769cbfa7d45f
Mahmoud AlQataa <malqataa2@gmail.com>
2026-08-07 18:49:19 +0300

    feat: integrate Flask-Migrate over SQLite

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from extensions import db
 from routes import main_bp
 from services import get_free_port
+from flask_migrate import Migrate
 
 
 def create_app():
@@ -14,11 +15,12 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # stop the tracking we don't need it yet
 
     db.init_app(app)  # (Initialize SQLAlchemy) creat the db and connect it to the app
+    migrate = Migrate(app, db)
 
     app.register_blueprint(main_bp)
 
-    with app.app_context():  # Enter the Flask application context. (# go in the env)
-        db.create_all()  # Create all tables defined by the models (if they don't already exist).
+    # with app.app_context():  # Enter the Flask application context. (# go in the env)
+    #     db.create_all()  # Create all tables defined by the models (if they don't already exist).
 
     return app
 
